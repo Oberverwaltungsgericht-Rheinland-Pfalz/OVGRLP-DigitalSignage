7dd3700b2a7cf811d463ac85736126d727b80f53
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2015-07-02 15:13:40 +0200

    Routing und Bereiche aktualisiert closes #18

--- src/DigitalSignage.WebUi.RoomControl/app/app.js
+++ src/DigitalSignage.WebUi.RoomControl/app/app.js
@@ -11,17 +11,25 @@
         $rootScope.$stateParams = $stateParams;
     }]);
 
-    app.config(function ($stateProvider, $urlRouterProvider, $mdThemingProvider, RestangularProvider) {
+    app.config(function ($stateProvider, $urlRouterProvider, $mdThemingProvider, $mdIconProvider, RestangularProvider) {
         $stateProvider
             .state('rooms', {
                 url: '/',
                 templateUrl: 'app/rooms.html',
                 controller: 'RoomsController'
-            })
-            .state('room', {
+            }).state('room', {
+                abstract: true,
                 url: '/:id',
                 templateUrl: 'app/room.html',
                 controller: 'RoomController'
+            }).state('room.terms', {
+                url: '/terms',
+                templateUrl: 'app/terms.html',
+                controller: 'RoomTermsController'
+            }).state('room.display', {
+                url: '/display',
+                templateUrl: 'app/display.html',
+                controller: 'RoomDisplayController'
             });
 
         $urlRouterProvider.otherwise('/');
@@ -29,10 +37,64 @@
         RestangularProvider.setBaseUrl('http://localhost:52208');
 
         $mdThemingProvider.theme('default')
-            .primaryPalette('grey')
-            .accentPalette('green');
+            .primaryPalette('indigo')
+            .accentPalette('red');
+
+        $mdIconProvider
+            .icon('menu', 'assets/icons/ic_menu_black_48px.svg')
+            .icon('save', 'assets/icons/ic_save_black_48px.svg')
+            .icon('terms', 'assets/icons/ic_event_note_black_48px.svg')
+            .icon('display', 'assets/icons/ic_dvr_black_48px.svg');
     });
 
+
+    app.controller('RoomsController', function ($scope, Restangular) {
+        $scope.displays = [];
+
+        var baseDisplays = Restangular.all('settings/displays');
+
+        baseDisplays.getList().then(function (data) {
+            $scope.displays = data;
+        });
+    });
+
+    app.controller('RoomController', function ($scope, $stateParams, Restangular) {
+        $scope.baseDisplay = Restangular.one('settings/displays', $stateParams.id);
+        $scope.display = $scope.baseDisplay.get().$object;
+    });
+
+    app.controller('RoomTermsController', function ($scope, $filter, $stateParams, $mdToast, Restangular) {
+        $scope.termine = [];
+
+        $scope.baseDisplay.getList('termine').then(function (data) {
+            $scope.termine = data;
+        });
+
+        $scope.save = function () {
+
+
+            $mdToast.show($mdToast.simple()
+                .content('Gespeichert!')
+                .position('top left')
+                .hideDelay(4000)
+            );
+        };
+
+        /*
+        var TermineSrv = Restangular.service('termine', Restangular.one('settings/displays', $stateParams.id));
+        TermineSrv = Restangular.service('termine', $scope.display);
+
+        TermineSrv.getList().then(function (data) {
+            $scope.termine = $filter('orderBy')(data, '+uhrzeitAktuell');
+        });
+        */
+    });
+
+    app.controller('RoomDisplayController', function ($scope, $stateParams) {
+        $scope.id = $stateParams.id;
+    });
+
+    /*
     app.controller('RoomsController', function ($scope, Displays) {
         $scope.rooms = null;
 
@@ -44,7 +106,9 @@
             $scope.rooms = data;
         });
     });
+    */
 
+    /*
     app.controller('RoomController', function ($scope, $filter, $stateParams, Restangular) {
         var TermineSrv = Restangular.service('termine', Restangular.one('settings/displays', $stateParams.id));
 
@@ -57,7 +121,7 @@
         };
 
         updateData();
-    });
+    });*/
 
     app.factory('Displays', function (Restangular) {
         Restangular.extendModel('settings/displays', function (model) {
