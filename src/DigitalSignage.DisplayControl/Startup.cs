ab1c40f78c727e09f83b4fd02734485b68f3e225
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2015-07-03 10:21:44 +0200

    Basisprojekt wurde angelegt closes #21

---
+++ src/DigitalSignage.DisplayControl/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Owin;
+using System.Web.Http;
+using Microsoft.Owin.StaticFiles;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin;
+using System.Reflection;
+using System.IO;
+using System;
+
+namespace DigitalSignage.DisplayControl
+{
+  public class Startup
+  {
+    public void Configuration(IAppBuilder appBuilder)
+    {
+      HttpConfiguration config = new HttpConfiguration();
+      config.MapHttpAttributeRoutes();
+
+      appBuilder.UseWebApi(config);
+
+      appBuilder.UseFileServer(new FileServerOptions()
+      {
+        EnableDefaultFiles = true,
+        EnableDirectoryBrowsing = false,
+        RequestPath = new PathString(""),
+        FileSystem = new PhysicalFileSystem(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "web"))
+      });
+    }
+  }
+}
