9bb64f80e9daa45c5f4ef3ebc8fb2424c35c7f3a
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2015-07-06 15:54:00 +0200

    Notwendige funktionen in WebApi implementiert und die RemoteControl WebUi implementiert closes #29

--- src/DigitalSignage.WebApi/Controllers/Settings/DisplaysController.cs
+++ src/DigitalSignage.WebApi/Controllers/Settings/DisplaysController.cs
@@ -9,6 +9,7 @@ using System.Data.Entity;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
@@ -16,156 +17,178 @@ using System.Web.Http.Description;
 
 namespace DigitalSignage.WebApi.Controllers.Settings
 {
-    [RoutePrefix("settings/displays")]
-    public class DisplaysController : ApiController
+  [RoutePrefix("settings/displays")]
+  public class DisplaysController : ApiController
+  {
+    private readonly DigitalSignageDbContext context = new DigitalSignageDbContext();
+    private readonly DisplayManagementService displayManagementService = new DisplayManagementService();
+
+    [Route("")]
+    [HttpGet]
+    public IEnumerable<Display> GetAllDisplays()
     {
-        private readonly DigitalSignageDbContext context = new DigitalSignageDbContext();
-        private readonly DisplayManagementService displayManagementService = new DisplayManagementService();
-
-        [Route("")]
-        [HttpGet]
-        public IEnumerable<Display> GetAllDisplays()
-        {
-            return context.Displays;
-        }
-
-        [Route("{id}/termine")]
-        [HttpGet]
-        [ResponseType(typeof(IEnumerable<VerfahrenDto>))]
-        public async Task<IHttpActionResult> GetAllTermine(int id)
-        {
-            var display = await context.Displays.FindAsync(id);
-
-            if (display == null)
-                return NotFound();
-
-            List<VerfahrenDto> dtos = new List<VerfahrenDto>();
-
-            context.Verfahren
-                .SqlQuery("SELECT * FROM Verfahren" + (!String.IsNullOrEmpty(display.Filter) ? " WHERE " + display.Filter : ""))
-                .ToList()
-                .ForEach(v =>
-                    {
-                        context.Entry(v).Reference("Stammdaten").Load();
-                        context.Entry(v).Collection("ParteienAktiv").Load();
-                        context.Entry(v).Collection("ParteienPassiv").Load();
-                        context.Entry(v).Collection("Besetzung").Load();
-                        context.Entry(v).Collection("ProzBevAktiv").Load();
-                        context.Entry(v).Collection("ProzBevPassiv").Load();
-                        context.Entry(v).Collection("ParteienBeigeladen").Load();
-                        context.Entry(v).Collection("ProzBevBeigeladen").Load();
-                        context.Entry(v).Collection("ParteienZeugen").Load();
-                        context.Entry(v).Collection("ParteienSV").Load();
-                        dtos.Add(VerfahrenController.GetDtoFromVerfahren(v));
-                    }
-            );
-
-            return Ok(dtos);
-        }
-
-        [Route("{id}", Name = "GetDisplayById")]
-        [HttpGet]
-        [ResponseType(typeof(Display))]
-        public async Task<IHttpActionResult> GetDisplay(int id)
-        {
-            var display = await context.Displays.FindAsync(id);
-
-            if (display == null)
-                return NotFound();
-
-            return Ok(display);
-        }
-
-        [Route("{id}/status")]
-        [HttpGet]
-        [ResponseType(typeof(DisplayStatus))]
-        public async Task<IHttpActionResult> GetStatusForDisplay(int id)
-        {
-            var display = await context.Displays.FindAsync(id);
-
-            if (display == null)
-                return NotFound();
-
-            return Ok(displayManagementService.GetDisplayStatus(display));
-        }
-
-        [Route("{id}")]
-        [HttpPut]
-        [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> PutDisplay(int id, Display display)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != display.Id)
-            {
-                return BadRequest();
-            }
-
-            try
-            {
-                context.Entry(display).State = EntityState.Modified;
-                await context.SaveChangesAsync();
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex);
-                return InternalServerError(ex);
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        [Route("")]
-        [HttpPost]
-        [ResponseType(typeof(Display))]
-        public async Task<IHttpActionResult> PostDisplay(Display display)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            try
-            {
-                context.Displays.Add(display);
-                await context.SaveChangesAsync();
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex);
-                return InternalServerError(ex);
-            }
-
-            return CreatedAtRoute("GetDisplayById", new { id = display.Id }, display);
-        }
-
-        [Route("{id}")]
-        [HttpDelete]
-        [ResponseType(typeof(Display))]
-        public async Task<IHttpActionResult> DeleteDisplay(int id)
-        {
-            var display = await context.Displays.FindAsync(id);
-
-            if (display == null)
-            {
-                return NotFound();
-            }
-
-            context.Displays.Remove(display);
-            await context.SaveChangesAsync();
-
-            return Ok(display);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-                context.Dispose();
-
-            base.Dispose(disposing);
-        }
+      return context.Displays;
     }
+
+    [Route("{id}", Name = "GetDisplayById")]
+    [HttpGet]
+    [ResponseType(typeof(Display))]
+    public async Task<IHttpActionResult> GetDisplay(int id)
+    {
+      var display = await context.Displays.FindAsync(id);
+
+      if (display == null)
+        return NotFound();
+
+      return Ok(display);
+    }
+
+    [Route("{id}/termine")]
+    [HttpGet]
+    [ResponseType(typeof(IEnumerable<VerfahrenDto>))]
+    public async Task<IHttpActionResult> GetAllTermine(int id)
+    {
+      var display = await context.Displays.FindAsync(id);
+
+      if (display == null)
+        return NotFound();
+
+      List<VerfahrenDto> dtos = new List<VerfahrenDto>();
+
+      context.Verfahren
+          .SqlQuery("SELECT * FROM Verfahren" + (!String.IsNullOrEmpty(display.Filter) ? " WHERE " + display.Filter : ""))
+          .ToList()
+          .ForEach(v =>
+              {
+                context.Entry(v).Reference("Stammdaten").Load();
+                context.Entry(v).Collection("ParteienAktiv").Load();
+                context.Entry(v).Collection("ParteienPassiv").Load();
+                context.Entry(v).Collection("Besetzung").Load();
+                context.Entry(v).Collection("ProzBevAktiv").Load();
+                context.Entry(v).Collection("ProzBevPassiv").Load();
+                context.Entry(v).Collection("ParteienBeigeladen").Load();
+                context.Entry(v).Collection("ProzBevBeigeladen").Load();
+                context.Entry(v).Collection("ParteienZeugen").Load();
+                context.Entry(v).Collection("ParteienSV").Load();
+                dtos.Add(VerfahrenController.GetDtoFromVerfahren(v));
+              }
+      );
+
+      return Ok(dtos);
+    }
+
+    [Route("{id}/status")]
+    [HttpGet]
+    [ResponseType(typeof(DisplayStatus))]
+    public async Task<IHttpActionResult> GetStatusForDisplay(int id)
+    {
+      var display = await context.Displays.FindAsync(id);
+
+      if (display == null)
+        return NotFound();
+
+      return Ok(displayManagementService.GetDisplayStatus(display));
+    }
+
+    [Route("{id}/start")]
+    [HttpGet]
+    [ResponseType(typeof(void))]
+    public async Task<IHttpActionResult> StartDisplay(int id)
+    {
+      var display = await context.Displays.FindAsync(id);
+
+      if (display == null)
+        return NotFound();
+
+      try
+      {
+        displayManagementService.StartDisplay(display);
+      }
+      catch
+      {
+        return InternalServerError();
+      }
+
+      return Ok();
+    }
+
+    [Route("{id}")]
+    [HttpPut]
+    [ResponseType(typeof(void))]
+    public async Task<IHttpActionResult> PutDisplay(int id, Display display)
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      if (id != display.Id)
+      {
+        return BadRequest();
+      }
+
+      try
+      {
+        context.Entry(display).State = EntityState.Modified;
+        await context.SaveChangesAsync();
+      }
+      catch (Exception ex)
+      {
+        Debug.WriteLine(ex);
+        return InternalServerError(ex);
+      }
+
+      return StatusCode(HttpStatusCode.NoContent);
+    }
+
+    [Route("")]
+    [HttpPost]
+    [ResponseType(typeof(Display))]
+    public async Task<IHttpActionResult> PostDisplay(Display display)
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      try
+      {
+        context.Displays.Add(display);
+        await context.SaveChangesAsync();
+      }
+      catch (Exception ex)
+      {
+        Debug.WriteLine(ex);
+        return InternalServerError(ex);
+      }
+
+      return CreatedAtRoute("GetDisplayById", new { id = display.Id }, display);
+    }
+
+    [Route("{id}")]
+    [HttpDelete]
+    [ResponseType(typeof(Display))]
+    public async Task<IHttpActionResult> DeleteDisplay(int id)
+    {
+      var display = await context.Displays.FindAsync(id);
+
+      if (display == null)
+      {
+        return NotFound();
+      }
+
+      context.Displays.Remove(display);
+      await context.SaveChangesAsync();
+
+      return Ok(display);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing)
+        context.Dispose();
+
+      base.Dispose(disposing);
+    }
+  }
 }
---
+++ src/DigitalSignage.WebUi.RoomControl/app/app.config.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    angular.module('app')
+        .config(function ($mdThemingProvider, $mdIconProvider, RestangularProvider) {
+            RestangularProvider.setBaseUrl('http://localhost:52208');
+
+            $mdThemingProvider.theme('default')
+                .primaryPalette('indigo')
+                .accentPalette('red');
+
+            $mdIconProvider
+                .icon('menu', 'assets/icons/ic_menu_black_48px.svg')
+                .icon('save', 'assets/icons/ic_save_black_48px.svg')
+                .icon('terms', 'assets/icons/ic_event_note_black_48px.svg')
+                .icon('display', 'assets/icons/ic_dvr_black_48px.svg')
+                .icon('power', 'assets/icons/ic_power_settings_new_black_48px.svg')
+                .icon('restart', 'assets/icons/ic_replay_black_48px.svg')
+                .icon('refresh', 'assets/icons/ic_refresh_black_48px.svg');
+        });
+})();
