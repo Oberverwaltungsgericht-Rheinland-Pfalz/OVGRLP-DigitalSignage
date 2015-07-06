eda73c9a527f1f5b2efc5f761adc846467c81e01
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2015-07-06 08:42:50 +0200

    Funktionen in WebInterface implementiert closes #24

---
+++ src/DigitalSignage.DisplayControl/web/app/app.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    var app = angular.module('app', [
+        // Custom modules
+
+        // 3rd Party Modules
+        'ngMaterial'
+    ]);
+
+    /*
+    app.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+        
+    }]);
+    */
+
+    app.config(function ($mdThemingProvider, $mdIconProvider) {
+        $mdThemingProvider.theme('default')
+           .primaryPalette('brown')
+           .accentPalette('red');
+
+        $mdIconProvider
+            .icon('shutdown', 'assets/icons/ic_power_settings_new_black_48px.svg')
+            .icon('restart', 'assets/icons/ic_replay_black_48px.svg')
+            .icon('refresh', 'assets/icons/ic_refresh_black_48px.svg');
+    });
+
+    app.controller('DisplayController', function ($scope, $http) {
+        $scope.screenshot = '../../api/screenshot?dt=' + new Date().getTime();
+
+        $scope.restart = function () {
+            $http.get('../../api/restart');
+        };
+
+        $scope.shutdown = function () {
+            $http.get('../../api/shutdown');
+        };
+
+        $scope.refresh = function () {
+            $scope.screenshot = '../../api/screenshot?dt=' + new Date().getTime();
+        };
+    });
+
+})();
