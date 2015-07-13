0fe56f4c072d6eb3751397b36298a1539773c3ab
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2015-07-13 15:08:10 +0200

    DSM: Detailsseite implementiert closes #37

--- src/DigitalSignage.WebUi.Manager/app/app.js
+++ src/DigitalSignage.WebUi.Manager/app/app.js
@@ -40,7 +40,7 @@
                 templateUrl: 'app/terms/details.html',
                 controller: 'TermDetailsController'
             });
- 
+
         $urlRouterProvider.otherwise('/displays');
 
         RestangularProvider.setBaseUrl('http://localhost:52208');
@@ -56,6 +56,10 @@
             .icon('display', 'assets/icons/ic_dvr_black_48px.svg')
             .icon('power', 'assets/icons/ic_power_settings_new_black_48px.svg')
             .icon('restart', 'assets/icons/ic_replay_black_48px.svg')
-            .icon('refresh', 'assets/icons/ic_refresh_black_48px.svg');
-            });
+            .icon('refresh', 'assets/icons/ic_refresh_black_48px.svg')
+            .icon('delete', 'assets/icons/ic_delete_black_24px.svg')
+            .icon('person', 'assets/icons/ic_person_black_24px.svg')
+            .icon('personadd', 'assets/icons/ic_person_add_black_24px.svg');
+    });
+
 })();
