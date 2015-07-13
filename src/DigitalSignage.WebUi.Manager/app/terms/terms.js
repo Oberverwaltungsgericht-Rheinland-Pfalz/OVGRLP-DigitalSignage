405600bdc0d2928eb2d3c87ea478059925db26bc
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2015-07-13 13:27:09 +0200

    DSM: Uebersicht fuer Sitzungen implementiert closes #36

--- src/DigitalSignage.WebUi.Manager/app/terms/terms.js
+++ src/DigitalSignage.WebUi.Manager/app/terms/terms.js
@@ -1,36 +1,40 @@
 ﻿(function () {
     var app = angular.module('app.terms');
 
-    app.controller('TermsController', ['$scope', '$stateParams', '$filter', 'Terms', function($scope, $stateParams, $filter, Terms) {
-        /*
-        $scope.table = new ngTableParams({
-            page: 1,
-            count: 15,
-            sorting: {
-                uhrzeitAktuell: 'asc'
-            }
-        }, {
-            counts: [10, 15, 20, 30, 40],
-            total: 0,
-            getData: function ($defer, params) {
-                Terms.getList().then(function (terms) {
-                    var filteredTerms = params.filter() ? $filter('filter')(terms, params.filter()) : terms
-                    var orderedTerms = params.sorting() ? $filter('orderBy')(filteredTerms, params.orderBy()) : filteredTerms;
+    app.controller('TermsController', function ($scope, $stateParams, $filter, Restangular) {
+        var baseTerms = Restangular.all('daten/verfahren');
+        var defaultSort =[
+            {field: 'uhrzeitAktuell', sort: 'asc'}
+        ];
 
-                    params.total(orderedTerms.length);
-                    $defer.resolve(orderedTerms.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-                });
-            }
-        });
+        var columnDefs = [
+            { headerName: 'Plan', headerGroup: 'Uhrzeit', width: 80, suppressSizeToFit: true, field: 'uhrzeitPlan' },
+            { headerName: 'Aktuell', headerGroup: 'Uhrzeit', width: 80, suppressSizeToFit: true, field: 'uhrzeitAktuell' },
+            { headerName: 'Aktenzeichen', width: 150, suppressSizeToFit: true, field: 'az' },
+            { headerName: 'Status', width: 150, suppressSizeToFit: true, field: 'status' },
+            { headerName: 'Aktiv', headerGroup: 'Parteien', suppressSorting: true, suppressMenu: true, template: '<span ng-repeat="partei in data.parteienAktiv">{{partei}}</span>' },
+            { headerName: 'Passiv', headerGroup: 'Parteien', suppressSorting: true, suppressMenu: true, template: '<span ng-repeat="partei in data.parteienPassiv">{{partei}}</span>' },
+            { headerName: 'Datum', width: 100, suppressSizeToFit: true, field: 'datum' },
+            { headerName: '', width: 100, suppressSizeToFit: true, suppressSorting: true, suppressMenu: true, template: '<a ui-sref="term({id:data.id})">Bearbeiten</a>' }
+        ];
 
-        $scope.reload = function () {
-            $scope.table.reload();
+        $scope.gridOptions = {
+            angularCompileRows: true,            enableSorting: true,            enableFilter: true,            columnDefs: columnDefs,
+            rowData: null,
+            groupHeaders: true,            groupKeys: ['sitzungssaal'],            groupUseEntireRow: true,            ready: function (api) {
+                api.sizeColumnsToFit();
+                api.setSortModel(defaultSort);
+            }
         };
-        */
-    }]);
 
-    app.controller('TermController', ['$scope', 'term', function ($scope, term) {
-        $scope.term = term;
+        baseTerms.getList().then(function (data) {
+            $scope.gridOptions.rowData = data;
+            $scope.gridOptions.api.onNewRows();
+        });
+    });
+
+    app.controller('TermDetailsController', ['$scope', 'term', function ($scope, $stateParams, Restangular) {
+        console.log('load' + $stateParams.id);
     }]);
 
     app.factory('Terms', ['Restangular', function (Restangular) {
