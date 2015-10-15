c2658d0becbce4263e21e73c852e6b436f74b38c
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2015-10-15 12:58:36 +0200

    Ansicht geaendert - App umstrukturiert - Filter nach Gruppe implementiert

--- src/DigitalSignage.WebUi.RoomControl/app/app.config.js
+++ src/DigitalSignage.WebUi.RoomControl/app/app.config.js
@@ -1,19 +1,27 @@
 ﻿(function () {
-    angular.module('app')
-        .config(function ($mdThemingProvider, $mdIconProvider, RestangularProvider) {
-            RestangularProvider.setBaseUrl('http://localhost:52208');
+  'use strict';
 
-            $mdThemingProvider.theme('default')
-                .primaryPalette('indigo')
-                .accentPalette('red');
+  angular
+    .module('app')
+    .config(config);
 
-            $mdIconProvider
-                .icon('menu', 'assets/icons/ic_menu_black_48px.svg')
-                .icon('save', 'assets/icons/ic_save_black_48px.svg')
-                .icon('terms', 'assets/icons/ic_event_note_black_48px.svg')
-                .icon('display', 'assets/icons/ic_dvr_black_48px.svg')
-                .icon('power', 'assets/icons/ic_power_settings_new_black_48px.svg')
-                .icon('restart', 'assets/icons/ic_replay_black_48px.svg')
-                .icon('refresh', 'assets/icons/ic_refresh_black_48px.svg');
-        });
+  config.$inject = ['$mdThemingProvider', '$mdIconProvider', 'RestangularProvider']
+
+  function config($mdThemingProvider, $mdIconProvider, RestangularProvider) {
+    RestangularProvider.setBaseUrl('http://localhost:52208');
+
+    $mdThemingProvider.theme('default')
+      .primaryPalette('indigo')
+      .accentPalette('red');
+
+    $mdIconProvider
+      .icon('menu', 'assets/icons/ic_menu_black_48px.svg')
+      .icon('home', 'assets/icons/ic_home_black_48px.svg')
+      .icon('save', 'assets/icons/ic_save_black_48px.svg')
+      .icon('terms', 'assets/icons/ic_event_note_black_48px.svg')
+      .icon('display', 'assets/icons/ic_dvr_black_48px.svg')
+      .icon('power', 'assets/icons/ic_power_settings_new_black_48px.svg')
+      .icon('restart', 'assets/icons/ic_replay_black_48px.svg')
+      .icon('refresh', 'assets/icons/ic_refresh_black_48px.svg');
+  }
 })();
