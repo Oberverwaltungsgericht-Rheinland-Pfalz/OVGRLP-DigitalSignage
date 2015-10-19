9a8db5e997f69bf4e46283e15ecd4a29f8421fd5
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2015-10-19 10:30:06 +0200

    WebApi um BreezeController erweitert

---
+++ src/DigitalSignage.WebApi/Controllers/EurekaDatenController.cs
@@ -0,0 +1,38 @@
+﻿using Breeze.ContextProvider;
+using Breeze.ContextProvider.EF6;
+using Breeze.WebApi2;
+using DigitalSignage.Infrastructure.Models.EurekaFach;
+using DigitalSignage.WebApi.Data;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Web.Http;
+
+namespace DigitalSignage.WebApi.Controllers
+{
+  [BreezeController]
+  public class EurekaDatenController : ApiController
+  {
+    private readonly EFContextProvider<DigitalSignageDbContext> contextProvider = new EFContextProvider<DigitalSignageDbContext>();
+
+    // ~/breeze/EurekaDaten/Metadata
+    [HttpGet]
+    public string Metadata()
+    {
+      return contextProvider.Metadata();
+    }
+
+    // ~/breeze/EurekaDaten/Verfahren
+    [HttpGet]
+    public IQueryable<Verfahren> Verfahren()
+    {
+      return contextProvider.Context.Verfahren;
+    }
+
+    // ~/breeze/EurekaDaten/SaveChanges
+    [HttpPost]
+    public SaveResult SaveChanges(JObject saveBundle)
+    {
+      return contextProvider.SaveChanges(saveBundle);
+    }
+  }
+}
