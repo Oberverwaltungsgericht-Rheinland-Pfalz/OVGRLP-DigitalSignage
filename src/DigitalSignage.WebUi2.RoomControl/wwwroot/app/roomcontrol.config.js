8ceef0743dd95078fcb2d403d591e44ce02a759e
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2015-11-24 10:13:23 +0100

    Manageraufruf fuer Termindetails - Link in Aktenzeichen eingefuegt - globale Konfiguration erweitert

--- src/DigitalSignage.WebUi2.RoomControl/wwwroot/app/roomcontrol.config.js
+++ src/DigitalSignage.WebUi2.RoomControl/wwwroot/app/roomcontrol.config.js
@@ -6,6 +6,8 @@
     .module('app')
     .constant('appConfig', {
       apiUrl: 'http://localhost:52208',
+      termDetailsUrl: 'http://localhost:51445/#/terms/',
+      showTermDetails: false,
       showHome: false
     });
 
