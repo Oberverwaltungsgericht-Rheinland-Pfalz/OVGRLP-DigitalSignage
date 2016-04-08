4b869ffc3fbc99de7272ed496b0fed65fb0d4c8b
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2016-04-08 08:59:46 +0200

    Umstellung auf separate Datenschicht abgeschlossen

--- src/DigitalSignage.WebApi/Migrations/Configuration.cs
+++ src/DigitalSignage.WebApi/Migrations/Configuration.cs
@@ -1,31 +1,31 @@
 namespace DigitalSignage.WebApi.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+  using System;
+  using System.Data.Entity;
+  using System.Data.Entity.Migrations;
+  using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<DigitalSignage.WebApi.Data.DigitalSignageDbContext>
+  internal sealed class Configuration : DbMigrationsConfiguration<DigitalSignage.Data.DigitalSignageDbContext>
+  {
+    public Configuration()
     {
-        public Configuration()
-        {
-            AutomaticMigrationsEnabled = false;
-        }
-
-        protected override void Seed(DigitalSignage.WebApi.Data.DigitalSignageDbContext context)
-        {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-        }
+      AutomaticMigrationsEnabled = false;
     }
+
+    protected override void Seed(DigitalSignage.Data.DigitalSignageDbContext context)
+    {
+      //  This method will be called after migrating to the latest version.
+
+      //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+      //  to avoid creating duplicate seed data. E.g.
+      //
+      //    context.People.AddOrUpdate(
+      //      p => p.FullName,
+      //      new Person { FullName = "Andrew Peters" },
+      //      new Person { FullName = "Brice Lambson" },
+      //      new Person { FullName = "Rowan Miller" }
+      //    );
+      //
+    }
+  }
 }
