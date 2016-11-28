0e1cdd1fdd2839a77d1598414946f84431430fe2
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2016-11-28 12:50:51 +0100

    WebUi2.Infoscreen: grunt ueberarbeitet und browserSynch implementiert, Probleme mit lodash behoben

---
+++ src/DigitalSignage.WebUi2.Infoscreen/wwwroot/app/infoscreen.config.js
@@ -0,0 +1,13 @@
+/*! DigitalSignage.WebUi2.Infoscreen - v2.2.0-1647 - 28.11.2016 */
+(function () {
+  'use strict';
+
+  angular
+    .module('ds-infoscreen')
+    .config(config);
+
+  config.$inject = [];
+
+  function config() {
+  }
+})();
