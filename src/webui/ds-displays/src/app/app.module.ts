1eb772ef4cb9d70f26fb964a3f4a077d435b5518
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2017-03-28 22:29:49 +0200

    home.component, display.component und Routing hinzugefügt

--- src/webui/ds-displays/src/app/app.module.ts
+++ src/webui/ds-displays/src/app/app.module.ts
@@ -2,14 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DisplayComponent } from './display/display.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: ':name', component: DisplayComponent },
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
