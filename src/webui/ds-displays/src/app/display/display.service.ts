ce4b9350e71f174635db0c1448fbba6b72d12a65
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2017-03-31 19:50:03 +0200

    Model für Display vervollständigt und vollständige Mockdaten aus Trier in DisplayService eingetragen

--- src/webui/ds-displays/src/app/display/display.service.ts
+++ src/webui/ds-displays/src/app/display/display.service.ts
@@ -1,15 +1,67 @@
 import { Injectable } from '@angular/core';
 import { Display } from './display'
 
+const DISPLAYS: Display[] = [
+  {
+    "id": 2,
+    "name": "5540DS-008",
+    "title": "Sitzungssaal I",
+    "template": "aushang-standard",
+    "styles": "aushang-1280x1024",
+    "filter": "Sitzungssaal = 'Sitzungssaal I'",
+    "group": "VGTR",
+    "controlUrl": "http://5540DS-008:9000",
+    "netAddress": "5540DS-008",
+    "wolIpAddress": "10.10.138.255",
+    "wolMacAddress": "00-04-5F-83-80-5B",
+    "wolUdpPort": 9,
+    "description": null,
+    "notes": null,
+    "dummy": false
+  },
+  {
+    "id": 3,
+    "name": "5540DS-004",
+    "title": "Sitzungssaal II",
+    "template": "aushang-standard",
+    "styles": "aushang-1280x1024",
+    "filter": "Sitzungssaal = 'Sitzungssaal II'",
+    "group": "VGTR",
+    "controlUrl": "http://5540DS-004:9000",
+    "netAddress": "5540DS-004",
+    "wolIpAddress": "10.10.138.255",
+    "wolMacAddress": "00-04-5F-89-7E-A4",
+    "wolUdpPort": 9,
+    "description": null,
+    "notes": null,
+    "dummy": false
+  },
+  {
+    "id": 7,
+    "name": "5540DS-002",
+    "title": "Sitzungssaal III",
+    "template": "aushang-standard",
+    "styles": "aushang-1280x1024",
+    "filter": "Sitzungssaal = 'Sitzungssaal III'",
+    "group": "VGTR",
+    "controlUrl": "http://5540DS-002:9000",
+    "netAddress": "5540DS-002",
+    "wolIpAddress": "10.10.138.255",
+    "wolMacAddress": "00-30-18-00-B4-4C",
+    "wolUdpPort": 9,
+    "description": null,
+    "notes": null,
+    "dummy": false
+  }
+];
+
+
 @Injectable()
 export class DisplayService {
 
   constructor() { }
 
   getDisplays(): Promise<Display[]> {
-    return Promise.resolve([
-      { id: 1, name: 'Test1' },
-      { id: 2, name: 'Test2' }
-    ]);
+    return Promise.resolve(DISPLAYS);
   }
 }
