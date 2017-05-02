a00cbcfa0e5597570e9844f40e33a88d293dd73a
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2017-05-02 09:09:08 +0200

    unfertige Änderungen

---
+++ src/webui/ds-core/src/ds-core.ts
@@ -0,0 +1,3 @@
+export { Display } from './model/display';
+export { Termin } from './model/termin';
+export { Config } from './model/config';
---
+++ src/webui/ds-core/src/services/config.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { Http } from '@angular/http';
+import { Config } from '../model/config';
+
+@Injectable()
+export class ConfigService {
+  private config: Config;
+
+  constructor(private http: Http) { }
+
+  load(url: string) {
+    return new Promise((resolve) => {
+      this.http.get(url).map(res => res.json())
+        .subscribe(config => {
+          this.config = config;
+          resolve();
+        });
+    })
+  }
+
+  getConfig(): Config {
+    return this.config;
+  }
+}
