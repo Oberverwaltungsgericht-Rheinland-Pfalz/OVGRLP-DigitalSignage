45b9a20d7835adc35dbe4cfb3d34155c8baaf1a5
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2017-05-04 15:24:47 +0200

    leere componenten für home und display angelegt

--- src/webui/ds-roomcontrol/src/app/app.module.ts
+++ src/webui/ds-roomcontrol/src/app/app.module.ts
@@ -2,14 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DisplayComponent } from './display/display.component';
+
+const appRoutes: Routes = [
+  { path: ':name', component: DisplayComponent },
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DisplayComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes, { useHash: true }),
     BrowserModule,
     FormsModule,
     HttpModule
