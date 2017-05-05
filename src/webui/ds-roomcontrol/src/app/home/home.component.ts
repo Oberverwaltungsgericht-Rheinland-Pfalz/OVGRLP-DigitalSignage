55bc5482003dc44ca14ac010d2c457e7f31e7ecf
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2017-05-05 21:27:38 +0200

    roomcontrol: verfügbare Displays auf Hauptseite auflisten

--- src/webui/ds-roomcontrol/src/app/home/home.component.ts
+++ src/webui/ds-roomcontrol/src/app/home/home.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Display, DisplayService } from 'ds-core';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [DisplayService]
 })
 export class HomeComponent implements OnInit {
+  displays: Display[];
 
-  constructor() { }
+  constructor(private displayService: DisplayService) { }
 
-  ngOnInit() {
+  getDisplays() {
+    this.displayService.getDisplays()
+      .subscribe(
+        displays => this.displays = displays.sort((d1, d2) => d1.title > d2.title ? 1 : -1)
+      );
   }
 
+  ngOnInit() {
+    this.getDisplays();
+  }
 }
