3f2d5c29347d508f48d76a099056732a7c373e45
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2017-05-16 21:32:07 +0200

    Bilder für verschiedene Stati hinzugefügt

--- src/webui/ds-roomcontrol/src/app/display-control/display-control.component.ts
+++ src/webui/ds-roomcontrol/src/app/display-control/display-control.component.ts
@@ -21,11 +21,22 @@ export class DisplayControlComponent implements OnInit {
     this.displayService.getDisplayStatus(this._display)
       .subscribe(response => {
         this.status = response
+        this.updateScreenshot();
       });
   }
   get display(): Display { return this._display; }
 
+  updateScreenshot() {
+    if(this.status === DisplayStatus.Offline)
+      this.screenshot = 'assets/img/offline.jpg'
+    else if(this.status === DisplayStatus.Online)
+      this.screenshot = `${this.display.controlUrl}/api/screenshot`;
+    else
+      this.screenshot = 'assets/img/unknown.jpg';
+  }
+
   ngOnInit() {
+    this.screenshot = 'assets/img/unknown.jpg';
   }
 
 }
