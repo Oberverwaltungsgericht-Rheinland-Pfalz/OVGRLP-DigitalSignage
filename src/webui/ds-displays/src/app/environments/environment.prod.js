c3d62d229962afea7085a2edd50f3430a84845e9
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2017-09-14 13:21:16 +0200

    ds-displays: js-Dateien aus src entfernt und Build-Skripte auf dest/ umgestellt

--- src/webui/ds-displays/src/app/environments/environment.prod.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = {
-    production: true,
-    configFile: 'config/config.json'
-};
-//# sourceMappingURL=environment.prod.js.map
