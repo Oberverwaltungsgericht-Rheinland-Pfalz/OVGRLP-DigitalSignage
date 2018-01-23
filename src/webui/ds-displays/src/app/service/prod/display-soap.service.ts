35ace86725194a88f7a64cfcd855dcae688429f0
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2018-01-23 11:42:16 +0100

    ds-displays: Bereinigungen in den Services

--- src/webui/ds-displays/src/app/service/prod/display-soap.service.ts
+++ src/webui/ds-displays/src/app/service/prod/display-soap.service.ts
@@ -53,19 +53,4 @@ export class DisplaySoapService implements DisplayService {
 
     return DisplayStatus.Unknown;
   }
-
-  /*
-  private handleError(error: HttpResponse<any> | any) {
-    let errMsg: string;
-    if (error instanceof HttpResponse) {
-      const body = error.ok .json() || '';
-      const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-    } else {
-      errMsg = error.message ? error.message : error.toString();
-    }
-    console.error(errMsg);
-    return Observable.throw(errMsg);
-  }
-  */
 }
--- src/webui/ds-displays/src/app/service/prod/termin-soap.service.ts
+++ src/webui/ds-displays/src/app/service/prod/termin-soap.service.ts
@@ -25,19 +25,4 @@ export class TerminSoapService implements TerminService {
     return this.http
       .get<Termin[]>(`${this.apiUrl}/settings/displays/${displayName}/termine`);
   }
-
-  /*
-  private handleError (error: Response | any) {
-    let errMsg: string;
-    if (error instanceof Response) {
-      const body = error.json() || '';
-      const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-    } else {
-      errMsg = error.message ? error.message : error.toString();
-    }
-    console.error(errMsg);
-    return Observable.throw(errMsg);
-  }
-  */
 }
