a6a67c807fb70c097e8993c5c82907cefbdd78f0
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2018-01-23 11:43:27 +0100

    ds-display: Template für NjzFoyer hinzugefügt

--- src/webui/ds-displays/src/templates/index.ts
+++ src/webui/ds-displays/src/templates/index.ts
@@ -2,17 +2,20 @@ import { NjzSaalTemplateComponent } from './njz-saal-template.component';
 import { EdvgtSteleTemplateComponent } from './edvgt-stele-template.component';
 import { NjzFoyerTemplateComponent } from './njz-foyer-template.component';
 import { NjzKhFoyerTemplateComponentUnten } from './njzkh-foyer-template-unten.component';
+import { NjzFoyer } from './njz-foyer';
 
 export const TEMPLATES: any[] = [
   NjzSaalTemplateComponent,
   EdvgtSteleTemplateComponent,
   NjzFoyerTemplateComponent,
-  NjzKhFoyerTemplateComponentUnten
+  NjzKhFoyerTemplateComponentUnten,
+  NjzFoyer
 ];
 
 export {
   NjzSaalTemplateComponent,
   EdvgtSteleTemplateComponent,
   NjzFoyerTemplateComponent,
-  NjzKhFoyerTemplateComponentUnten
+  NjzKhFoyerTemplateComponentUnten,
+  NjzFoyer
 };
