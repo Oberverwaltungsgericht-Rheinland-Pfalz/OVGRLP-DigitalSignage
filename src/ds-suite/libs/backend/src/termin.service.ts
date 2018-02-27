74c5a277497b8e26c5aec52244f9a99c1f7ec5e1
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2018-02-27 14:48:53 +0100

    ds-suite: termin.service implementiert

--- src/ds-suite/libs/backend/src/termin.service.ts
+++ src/ds-suite/libs/backend/src/termin.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 
-import { } from ''
+import { Display, Termin } from '@ds-suite/model';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+const WEBAPI_URL = "http://...";
 
 @Injectable()
 export class TerminService {
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTermine(displayName: string): Observable<Termin[]> {
+    return this.http
+      .get<Termin[]>(`${WEBAPI_URL}/settings/display/${displayName}/termine`);
+  }
 }
