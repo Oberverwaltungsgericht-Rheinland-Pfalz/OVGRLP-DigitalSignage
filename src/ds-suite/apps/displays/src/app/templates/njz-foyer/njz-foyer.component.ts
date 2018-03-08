2468a6e817968ad0fc38c9f1e34242ec6ed8b8f5
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2018-03-08 15:29:54 +0100

    ds-suite: Problem mit Animation in njz-foyer.component behoben (echt doof von mir)

--- src/ds-suite/apps/displays/src/app/templates/njz-foyer/njz-foyer.component.ts
+++ src/ds-suite/apps/displays/src/app/templates/njz-foyer/njz-foyer.component.ts
@@ -1,9 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition, stagger, query, keyframes } from '@angular/animations';
+
 import { DisplayTemplateComponent } from '../../display-template/display-template.component';
 
 @Component({
   selector: 'app-njz-foyer',
   templateUrl: './njz-foyer.component.html',
-  styleUrls: ['./njz-foyer.component.css']
+  styleUrls: ['./njz-foyer.component.css'],
+  animations: [
+    trigger('terminAnimation', [
+      state('in', style({ opacity: 1, height: '*', 'padding-top': '*' })),
+      transition('in => void', [
+        animate('2s ease-out',
+          keyframes([
+            style({ opacity: 0, offset: 0.3, 'padding-top': 0 }),
+            style({ height: 0, offset: 1 })
+          ])
+        )
+      ])
+    ])
+  ]
 })
-export class NjzFoyerComponent extends DisplayTemplateComponent {}
+export class NjzFoyerComponent extends DisplayTemplateComponent { }
