43bae42214af4d9f563d7952b1fd17bcc5359eb1
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2018-03-08 14:05:38 +0100

    ds-suite: Update Pakages

--- src/ds-suite/apps/displays/src/app/app.module.ts
+++ src/ds-suite/apps/displays/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { NxModule } from '@nrwl/nx';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NxModule } from '@nrwl/nx';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 import { BackendModule } from '@ds-suite/backend';
--- src/ds-suite/apps/roomcontrol/src/app/app.module.ts
+++ src/ds-suite/apps/roomcontrol/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NxModule } from '@nrwl/nx';
-import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [BrowserModule, NxModule.forRoot(), RouterModule.forRoot([], { initialNavigation: 'enabled' })],
