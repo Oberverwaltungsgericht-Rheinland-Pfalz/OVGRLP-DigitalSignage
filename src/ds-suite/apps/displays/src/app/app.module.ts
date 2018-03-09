da9640adf666d7bd1a16869402801ebdc56c9407
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2018-03-09 11:17:44 +0100

    ds-suite: displays - Backend abhängig von environment nutzen

--- src/ds-suite/apps/displays/src/app/app.module.ts
+++ src/ds-suite/apps/displays/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, enableProdMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Routes, RouterModule } from '@angular/router';
@@ -6,7 +6,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { NxModule } from '@nrwl/nx';
 
 import { BackendModule } from '@ds-suite/backend';
-import { BackendDevModule as BACKEND_MODULE } from '@ds-suite/backend-dev';
+import { BackendDevModule } from '@ds-suite/backend-dev';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -21,8 +21,15 @@ import { TemplateHostDirective } from './display-template/template-host.directiv
 import { TEMPLATES } from './templates';
 
 import { CapitalizePipe } from './capitalize.pipe';
+import { environment } from '../environments/environment';
 
 const routes: Routes = [{ path: '', component: HomeComponent }, { path: ':name', component: DisplayComponent }];
+
+let BACKEND_MODULE = BackendDevModule;
+if (environment.production) {
+  BACKEND_MODULE = BackendModule;
+  enableProdMode();
+}
 
 @NgModule({
   imports: [
