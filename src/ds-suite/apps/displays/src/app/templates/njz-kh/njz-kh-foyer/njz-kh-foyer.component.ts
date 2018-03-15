8420cdc74ae7285b892ace9b0143e3861a0b8b81
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2018-03-15 20:17:47 +0100

    ds-suite: displays - njz-kh-foyer.component implementiert und altes Template entfernt

--- src/ds-suite/apps/displays/src/app/templates/njz-kh/njz-kh-foyer/njz-kh-foyer.component.ts
+++ src/ds-suite/apps/displays/src/app/templates/njz-kh/njz-kh-foyer/njz-kh-foyer.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition, stagger, query, keyframes } from '@angular/animations';
+
 import { DisplayTemplateComponent } from '../../../display-template/display-template.component';
 
 @Component({
   selector: 'app-njz-kh-foyer',
   templateUrl: './njz-kh-foyer.component.html',
-  styleUrls: ['./njz-kh-foyer.component.css']
+  styleUrls: ['./njz-kh-foyer.component.css'],
+  animations: [
+    trigger('terminAnimation', [
+      state('in', style({ opacity: 1, height: '*', 'padding-top': '*' })),
+      transition('in => void', [
+        animate(
+          '2s ease-out',
+          keyframes([
+            style({ opacity: 0, offset: 0.3, 'padding-top': 0 }), 
+            style({ height: 0, offset: 1 })
+          ])
+        )
+      ])
+    ])
+  ]
 })
 export class NjzKhFoyerComponent extends DisplayTemplateComponent { }
