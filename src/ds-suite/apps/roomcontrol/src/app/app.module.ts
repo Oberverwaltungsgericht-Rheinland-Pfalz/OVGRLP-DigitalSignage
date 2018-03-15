3e7615d3d5ecf3e76b675cb78000912231baa36d
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2018-03-15 20:37:29 +0100

    ds-suite: rc - angefangen roomcontrol zu implementieren

--- src/ds-suite/apps/roomcontrol/src/app/app.module.ts
+++ src/ds-suite/apps/roomcontrol/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NxModule } from '@nrwl/nx';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [BrowserModule, NxModule.forRoot(), RouterModule.forRoot([], { initialNavigation: 'enabled' })],
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/ds-suite/apps/roomcontrol/src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Display } from '@ds-suite/model';
+import { DisplayService } from '@ds-suite/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  displays: Display[];
+
+  constructor(private displayService: DisplayService) { }
+
+  getDisplays() {
+    this.displayService.getDisplays()
+      .subscribe(
+        displays => this.displays = displays.sort((d1, d2) => d1.title > d2.title ? 1 : -1)
+      );
+  }
+
+  ngOnInit() {
+    this.getDisplays();
+  }
+}
