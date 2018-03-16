e8f2bca181a910fdaa1e6a19de6611f2bc61d08c
Tobias Grotheer <tobias.grotheer@ovg.mjv.rlp.de>
2018-03-16 12:53:30 +0100

    ds-displays: rc - Clarity als UI-Framework hinzugefügt

--- src/ds-suite/apps/roomcontrol/src/app/app.module.ts
+++ src/ds-suite/apps/roomcontrol/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NxModule } from '@nrwl/nx';
+import { ClarityModule } from "@clr/angular";
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [BrowserModule, NxModule.forRoot(), RouterModule.forRoot([], { initialNavigation: 'enabled' })],
+    ClarityModule,
   declarations: [AppComponent, HomeComponent],
   bootstrap: [AppComponent]
 })
--- src/ds-suite/apps/roomcontrol/src/main.ts
+++ src/ds-suite/apps/roomcontrol/src/main.ts
@@ -1,8 +1,12 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import "@webcomponents/custom-elements";
+import "@clr/icons";
+import "@clr/icons/shapes/all-shapes";
+
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
 
 if (environment.production) {
   enableProdMode();
