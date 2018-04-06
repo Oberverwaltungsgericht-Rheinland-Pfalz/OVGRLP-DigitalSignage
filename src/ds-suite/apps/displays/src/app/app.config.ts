e46284f846d5ce9324a662bfd13c7adc279711dc
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2018-04-06 11:50:56 +0200

    app.config wieder auf den lokalen Testserver umgestellt

--- src/ds-suite/apps/displays/src/app/app.config.ts
+++ src/ds-suite/apps/displays/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { AppConfig } from '@ds-suite/model';
 
 export const DS_DISPLAYS_CONFIG: AppConfig = {
-  webApiUrl: 'http://10.10.90.198:8084/webapi'
+  webApiUrl: 'http://localhost:4200'
 };
