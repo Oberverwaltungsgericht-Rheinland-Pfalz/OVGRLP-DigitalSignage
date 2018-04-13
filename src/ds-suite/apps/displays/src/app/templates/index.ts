f0af19b31d00e146ef6308f6f70af53e425b4a18
Stefan Meißner <stefan.meissner@zw.jm.rlp.de>
2018-04-13 13:28:12 +0200

    Erweiterung um die drei neuen Templatetypen "Foyer mit Wappen", "Saal mit weiteren Terminen rechts" und "Saal mit weiteren Terminen unten".

--- src/ds-suite/apps/displays/src/app/templates/index.ts
+++ src/ds-suite/apps/displays/src/app/templates/index.ts
@@ -1,6 +1,6 @@
 import { NjzKoFoyerComponent, NjzKoSaalComponent } from './njz-ko';
-import { NjzKhFoyerComponent, NjzKhSaalComponent } from './njz-kh';
+import { NjzKhFoyerComponent, NjzKhSaalComponent, NjzKhFoyerWappenComponent, NjzKhSaalWeitereRechtsComponent, NjzKhSaalWeitereUntenComponent } from './njz-kh';
 
-export const TEMPLATES: any[] = [NjzKoFoyerComponent, NjzKoSaalComponent, NjzKhFoyerComponent, NjzKhSaalComponent];
+export const TEMPLATES: any[] = [NjzKoFoyerComponent, NjzKoSaalComponent, NjzKhFoyerComponent, NjzKhSaalComponent, NjzKhFoyerWappenComponent, NjzKhSaalWeitereRechtsComponent, NjzKhSaalWeitereUntenComponent];
 export * from './njz-ko';
 export * from './njz-kh';
--- src/ds-suite/apps/displays/src/app/templates/njz-kh/index.ts
+++ src/ds-suite/apps/displays/src/app/templates/njz-kh/index.ts
@@ -1,2 +1,5 @@
 export { NjzKhFoyerComponent } from './njz-kh-foyer/njz-kh-foyer.component';
 export { NjzKhSaalComponent } from './njz-kh-saal/njz-kh-saal.component';
+export { NjzKhFoyerWappenComponent } from './njz-kh-foyer-wappen/njz-kh-foyer-wappen.component';
+export { NjzKhSaalWeitereRechtsComponent } from './njz-kh-saal-weitere-rechts/njz-kh-saal-weitere-rechts.component';
+export { NjzKhSaalWeitereUntenComponent } from './njz-kh-saal-weitere-unten/njz-kh-saal-weitere-unten.component';
