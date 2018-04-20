7e1d5b9a34cb7bbf0c100e98578d42f1a8dae542
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2018-04-20 09:08:14 +0200

    ds-suite: Template NjzKhFoyerWappen in njz-kh.module eingebunden und Testdisplay in json-server hinzugefügt

--- src/ds-suite/apps/displays/src/app/templates/njz-kh/njz-kh.module.ts
+++ src/ds-suite/apps/displays/src/app/templates/njz-kh/njz-kh.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { NjzKhFoyerComponent } from './njz-kh-foyer/njz-kh-foyer.component';
+import { NjzKhFoyerWappenComponent } from './njz-kh-foyer-wappen/njz-kh-foyer-wappen.component';
 import { NjzKhSaalComponent } from './njz-kh-saal/njz-kh-saal.component';
 
 import { DsCommonModule } from '../../ds-common/ds-common.module';
 
 const routes: Routes = [
   { path: "foyer", component: NjzKhFoyerComponent },
+  { path: "foyer-wappen", component: NjzKhFoyerWappenComponent },
   { path: "saal", component: NjzKhSaalComponent }
 ];
 
@@ -22,6 +24,7 @@ const routes: Routes = [
   ],
   declarations: [
     NjzKhFoyerComponent,
+    NjzKhFoyerWappenComponent,
     NjzKhSaalComponent
   ]
 })
