a7fcd42b418ff6deffff65538e2307edec52607e
Stefan Meißner <stefan.meissner@zw.jm.rlp.de>
2018-04-24 08:52:42 +0200

    Erweiterung um neue Templates "NjzKhSaalWeitereRechtsComponent" und "NjzKhSaalWeitereUntenComponent"

---
+++ src/ds-suite/apps/displays/src/app/templates/njz-kh/njz-kh-saal-weitere-unten/njz-kh-saal-weitere-unten.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition, stagger, query, keyframes } from '@angular/animations';
+
+import { DisplayTemplateComponent } from '../../../display-template/display-template.component';
+
+@Component({
+  selector: 'app-njz-kh-saal-weitere-unten',
+  templateUrl: './njz-kh-saal-weitere-unten.component.html',
+  styleUrls: ['./njz-kh-saal-weitere-unten.component.css'],
+  animations: [
+    trigger('terminAnimation', [
+      state('in', style({ opacity: 1, height: '*', 'padding-top': '*' })),
+      transition('in => void', [
+        animate(
+          '2s ease-out',
+          keyframes([
+            style({ opacity: 0, offset: 0.3, 'padding-top': 0 }), 
+            style({ height: 0, offset: 1 })
+          ])
+        )
+      ])
+    ])
+  ]
+})
+export class NjzKhSaalWeitereUntenComponent extends DisplayTemplateComponent {
+}
