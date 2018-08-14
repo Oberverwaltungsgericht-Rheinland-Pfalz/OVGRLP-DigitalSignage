9bf9435bd99e0d0eeb66fbae3551532ddd3f62e4
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2018-08-14 10:12:37 +0200

    ds-suite - rc: Clarity aktualisiert und in RoomControl eingebunden

--- src/ds-suite/apps/roomcontrol/src/main.ts
+++ src/ds-suite/apps/roomcontrol/src/main.ts
@@ -1,10 +1,6 @@
 import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import "@webcomponents/custom-elements";
-import "@clr/icons";
-import "@clr/icons/shapes/all-shapes";
-
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
 
