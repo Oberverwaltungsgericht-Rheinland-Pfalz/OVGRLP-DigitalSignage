8ba9a3e66540f1af5cdbc29662567f1ca48a8cb4
Christoph Hessel <Christoph.Hessel@ovg.jm.rlp.de>
2018-08-27 14:38:16 +0200

    ds-suite - manager: app.module, app.component angepasst; package.app.json hinzugefügt

--- src/ds-suite/apps/dsmanager/src/app/app.component.ts
+++ src/ds-suite/apps/dsmanager/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+const { version: appVersion } = require('../../package.app.json');
 
 @Component({
-  selector: 'ds-suite-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ds-suite';
+export class AppComponent implements OnInit {
+  appVersion: string = "";
+
+  constructor() {}
+
+  ngOnInit() {
+    this.appVersion = appVersion;
+  }
 }
--- src/ds-suite/apps/dsmanager/src/app/app.module.ts
+++ src/ds-suite/apps/dsmanager/src/app/app.module.ts
@@ -1,13 +1,45 @@
+import { NgModule,LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from "@angular/router";
+
+import { ClarityModule } from "@clr/angular";
+
+import { registerLocaleData } from "@angular/common";
+import localeDe from "@angular/common/locales/de";
+
+import { NxModule } from '@nrwl/nx';
+
+import { BackendModule } from "@ds-suite/backend";
+import { CoreModule } from '@ds-suite/core';
 
 import { AppComponent } from './app.component';
-import { NxModule } from '@nrwl/nx';
+
+
+registerLocaleData(localeDe);
+
+const routes: Routes = [
+  /*{ path: "", component: HomeComponent }*/
+];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NxModule.forRoot()],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [
+    BrowserModule, 
+    NxModule.forRoot(),
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      initialNavigation: "enabled"
+    }),
+    ClarityModule,
+    BackendModule,
+    CoreModule
+  ],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: "de"
+    }
+  ],
+  bootstrap: [AppComponent]  
 })
 export class AppModule {}
