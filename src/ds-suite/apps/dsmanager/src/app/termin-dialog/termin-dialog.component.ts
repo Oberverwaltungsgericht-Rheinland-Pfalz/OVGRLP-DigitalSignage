19916c5c65e1a3ef694f854a291af4c7c66fd4a0
Christoph Hessel <Christoph.Hessel@ovg.jm.rlp.de>
2018-09-07 09:18:34 +0200

    ds-suite - manager: termin-dialog.component, Daten zur Bearbeitung anzeigen - erster Entwurf

--- src/ds-suite/apps/dsmanager/src/app/termin-dialog/termin-dialog.component.ts
+++ src/ds-suite/apps/dsmanager/src/app/termin-dialog/termin-dialog.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Termin } from '@ds-suite/model';
+
 @Component({
   selector: 'termin-dialog',
   templateUrl: './termin-dialog.component.html',
   styleUrls: ['./termin-dialog.component.css']
 })
 export class TerminDialogComponent implements OnInit {
+  public termin: Termin;
+  public origTermin: Termin;
+  public show: boolean = false;
 
   constructor() { }
 
+  open(termin: Termin) {
+    this.origTermin = termin;
+    this.termin = (JSON.parse(JSON.stringify(termin)));  /*Hack: kopie von Objekt termin*/
+    this.show = true;
+  }
+
+  addItemClick(arr: Array<string>) {
+
+  }
+  
+  deleteItemClick(arr: Array<string>, element: string) {
+    var index = arr.indexOf(element, 0);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+  }
+
+  close() {
+    this.show = false;
+  }
+
   ngOnInit() {
+    console.log(this.termin)
   }
 
 }
