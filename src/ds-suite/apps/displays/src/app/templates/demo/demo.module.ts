dc39b4ae2acab1e461fab3f2c7cdecb044211252
Stefan Meißner <stefan.meissner@zw.jm.rlp.de>
2018-09-10 15:20:07 +0200

    Korrekturen und Erweiterung der Demoseiten.

--- src/ds-suite/apps/displays/src/app/templates/demo/demo.module.ts
+++ src/ds-suite/apps/displays/src/app/templates/demo/demo.module.ts
@@ -5,6 +5,8 @@ import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { DemoFoyerVarAComponent } from './foyer-var-a/foyer-var-a.component';
 import { DemoFoyerVarBComponent } from './foyer-var-b/foyer-var-b.component';
+import { DemoFoyerVarTrComponent } from './foyer-var-tr/foyer-var-tr.component';
+import { DemoFoyerVarTrierComponent } from './foyer-var-trier/foyer-var-trier.component';
 import { DemoSaalVarAComponent } from './saal-var-a/saal-var-a.component';
 import { DemoSaalVarBComponent } from './saal-var-b/saal-var-b.component';
 import { DemoSaalVarRechtsComponent } from './saal-var-weitere-rechts/saal-var-weitere-rechts.component';
@@ -17,6 +19,8 @@ import { DsCommonModule } from '../../ds-common/ds-common.module';
 const routes: Routes = [
   { path: "demo-foyer-variante-a", component: DemoFoyerVarAComponent },
   { path: "demo-foyer-variante-b", component: DemoFoyerVarBComponent },
+  { path: "demo-foyer-variante-tr", component: DemoFoyerVarTrComponent },
+  { path: "demo-foyer-variante-trier", component: DemoFoyerVarTrierComponent },
   { path: "demo-saal-variante-a", component: DemoSaalVarAComponent },
   { path: "demo-saal-variante-b", component: DemoSaalVarBComponent },
   { path: "demo-saal-variante-weitere-rechts", component: DemoSaalVarRechtsComponent },
@@ -35,6 +39,8 @@ const routes: Routes = [
   declarations: [
     DemoFoyerVarAComponent,
     DemoFoyerVarBComponent,
+    DemoFoyerVarTrComponent,
+    DemoFoyerVarTrierComponent,
     DemoSaalVarAComponent,
     DemoSaalVarBComponent,
     DemoSaalVarRechtsComponent,
