9674b1551035141228cb40c3fd185a1248030da1
Christoph Hessel <Christoph.Hessel@ovg.jm.rlp.de>
2018-09-10 09:28:02 +0200

    ds-suite - backend:  breeze hinzugefügt

--- src/ds-suite/libs/backend/src/lib/backend.module.ts
+++ src/ds-suite/libs/backend/src/lib/backend.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { BreezeBridgeHttpClientModule } from 'breeze-bridge2-angular';
+
 import { SoapDisplayService } from './services/soap-display.service';
 import { SoapTerminService } from './services/soap-termin.service';
 import { TerminService, DisplayService } from '@ds-suite/core';
 
 @NgModule({
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, BreezeBridgeHttpClientModule, HttpClientModule],
   providers: [
     { provide: TerminService, useClass: SoapTerminService },
     { provide: DisplayService, useClass: SoapDisplayService }
