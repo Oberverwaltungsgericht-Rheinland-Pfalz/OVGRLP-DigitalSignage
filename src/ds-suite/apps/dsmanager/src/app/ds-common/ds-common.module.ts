6c7e27b7775eabebb4074fd165b16a6e3781b14e
Christoph Hessel <Christoph.Hessel@ovg.jm.rlp.de>
2018-09-25 16:37:57 +0200

    ds-suite - core: HTTP_INTERCEPTORS per Factory laden (prototypische Implementation)

--- src/ds-suite/apps/dsmanager/src/app/ds-common/ds-common.module.ts
+++ src/ds-suite/apps/dsmanager/src/app/ds-common/ds-common.module.ts
@@ -3,8 +3,17 @@ import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { ConfigService } from '@ds-suite/core';
 import { JsonConfigService } from '@ds-suite/backend';
 
+import { HTTP_INTERCEPTORS, HttpInterceptor } from '@angular/common/http';
+import { CredentialsInterceptor,DefaultInterceptor } from '@ds-suite/core';
+
 export function ConfigLoader(configService: JsonConfigService) {
   return () => configService.load('./assets/config.json');
+}
+
+export function InterceptorLoader(configService: JsonConfigService): HttpInterceptor {
+  //return new DefaultInterceptor();
+  return new CredentialsInterceptor();     //configService.getConfig().webApiUrl;
+
 }
 
 @NgModule({
@@ -22,7 +31,13 @@ export function ConfigLoader(configService: JsonConfigService) {
     {
       provide: ConfigService,
       useExisting: JsonConfigService
-    },
+    },    
+    {
+      provide: HTTP_INTERCEPTORS,
+      useFactory: InterceptorLoader,
+      deps: [JsonConfigService],
+      multi: true
+    }
   ]
 })
 export class DsCommonModule { }
