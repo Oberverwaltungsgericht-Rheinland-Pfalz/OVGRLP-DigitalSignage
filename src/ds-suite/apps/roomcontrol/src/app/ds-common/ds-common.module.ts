706312cbdb2499bef2eb7acaab5fcd338c1a9e3b
Christoph Hessel <Christoph.Hessel@ovg.jm.rlp.de>
2018-09-26 16:30:28 +0200

    ds-suite - rc: setting useWindowsAuthentication berücksichtigen

--- src/ds-suite/apps/roomcontrol/src/app/ds-common/ds-common.module.ts
+++ src/ds-suite/apps/roomcontrol/src/app/ds-common/ds-common.module.ts
@@ -3,8 +3,23 @@ import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { ConfigService } from '@ds-suite/core';
 import { JsonConfigService } from '@ds-suite/backend';
 
+import { HTTP_INTERCEPTORS, HttpInterceptor } from '@angular/common/http';
+import { CredentialsInterceptor,DefaultInterceptor } from '@ds-suite/core';
+
 export function ConfigLoader(configService: JsonConfigService) {
   return () => configService.load('./assets/config.json');
+}
+
+export function InterceptorLoader(configService: JsonConfigService): HttpInterceptor {
+  /*
+    In dieser Factory ist der ConfigLoader (welcher im APP_INITIALIZER genutzt wird) noch nicht fertig,
+    sodass hier configServicegetConfig() immer undefined ist
+    da Angular bis jetzt keine asynchronen provider unterstützt - siehe auch Angular Issue #23279 - muss es
+    erst einmal so gelöst werden, dass im CredentialsInterceptor die Settings geprüft werden
+    //!\TODO: Prüfen ob es evtl. mittlerweile eine Möglichkeit gibt!!
+  */
+  //return new DefaultInterceptor();
+  return new CredentialsInterceptor(configService);
 }
 
 @NgModule({
@@ -22,7 +37,13 @@ export function ConfigLoader(configService: JsonConfigService) {
     {
       provide: ConfigService,
       useExisting: JsonConfigService
-    },
+    },    
+    {
+      provide: HTTP_INTERCEPTORS,
+      useFactory: InterceptorLoader,
+      deps: [JsonConfigService],
+      multi: true
+    }
   ]
 })
 export class DsCommonModule { }
