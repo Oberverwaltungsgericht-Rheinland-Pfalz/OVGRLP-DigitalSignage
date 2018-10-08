d73e5b8875211854d5b4189c63545f945a85cb80
Christoph Hessel <Christoph.Hessel@ovg.jm.rlp.de>
2018-10-08 12:16:57 +0200

    webApi: release 2.8.0.1841

--- src/DigitalSignage.WebApi/Properties/AssemblyInfo.cs
+++ src/DigitalSignage.WebApi/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Revisions- und Buildnummern
 // übernehmen, indem Sie "*" wie folgt verwenden:
-[assembly: AssemblyFileVersion("2.7.0.1840")]
+[assembly: AssemblyFileVersion("2.8.0.1841")]
