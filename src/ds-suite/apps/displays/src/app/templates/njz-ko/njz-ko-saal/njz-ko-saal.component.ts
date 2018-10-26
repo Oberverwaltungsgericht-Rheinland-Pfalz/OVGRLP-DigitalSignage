2017fd0c366d8fc3e399db0e6bfe29905a26aa44
Tobias Grotheer <tobias.grotheer@ovg.jm.rlp.de>
2018-10-26 10:18:06 +0200

    ds-suite - displays: njz-ko-saal.component Filterung der abgeschlossenen Termine, wenn mehr als 9 Termine in der Liste sind

--- src/ds-suite/apps/displays/src/app/templates/njz-ko/njz-ko-saal/njz-ko-saal.component.ts
+++ src/ds-suite/apps/displays/src/app/templates/njz-ko/njz-ko-saal/njz-ko-saal.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { DisplayTemplateComponent } from '../../../display-template/display-template.component';
+import { Termin, TerminStatus } from '@ds-suite/model';
 
 import { TerminFunctions } from '../shared/termin.functions';
+
+const MAX_TERMINE = 9;
+const MAX_FINISHED = 3;
 
 @Component({
   selector: 'app-njz-saal',
@@ -10,4 +14,10 @@ import { TerminFunctions } from '../shared/termin.functions';
 })
 export class NjzKoSaalComponent extends DisplayTemplateComponent {
   termFunc = TerminFunctions;
+
+  filterTermine(termine: Termin[]) : Termin[] {
+    return super.removeFinishedTermine(
+      super.sortTermine(super.filterTermine(termine)), 
+      MAX_TERMINE, MAX_FINISHED);
+  }
 }
