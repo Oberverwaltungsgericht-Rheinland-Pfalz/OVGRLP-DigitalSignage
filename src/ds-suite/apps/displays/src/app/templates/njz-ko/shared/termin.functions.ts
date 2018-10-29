472b174847e7f9e2c972451246534f5331b3fdc7
Christoph Hessel <Christoph.Hessel@ovg.jm.rlp.de>
2018-10-29 15:02:11 +0100

    ds-suite - displays: NJZ-ko foyer - Bemerkungsfeld anzeigen wenn keine Aktivparteien vorhanden sind

--- src/ds-suite/apps/displays/src/app/templates/njz-ko/shared/termin.functions.ts
+++ src/ds-suite/apps/displays/src/app/templates/njz-ko/shared/termin.functions.ts
@@ -1,6 +1,15 @@
 import { Termin } from "@ds-suite/model";
 
 export class TerminFunctions {
+
+  public static AnzahlAktivparteien(termin: Termin) {
+    return (termin.parteienAktiv.length);
+  }
+
+  public static AnzahlPassivparteien(termin: Termin) {
+    return (termin.parteienPassiv.length);
+  }
+
   public static IstPersonalvertretung(termin: Termin) {
     return termin.gericht === "Oberverwaltungsgericht Rheinland-Pfalz" &&
       (termin.kammer == 4 || termin.kammer == 5);
