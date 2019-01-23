5b8f682c258314bf5da0aa6d6fd5a698c7dddad6
Christoph Hessel <Christoph.Hessel@ovg.jm.rlp.de>
2019-01-23 14:12:42 +0100

    WebApi: migration Scripte für 2-9-0-1904 hinzugefügt

---
+++ src/DigitalSignage.Data/Migrations/201901231214380_2-9-0-1904_ObjekteAndBeteiligteAdded.cs
@@ -0,0 +1,65 @@
+namespace DigitalSignage.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _2901904_ObjekteAndBeteiligteAdded : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Objekte",
+                c => new
+                    {
+                        ObjektId = c.Int(nullable: false, identity: true),
+                        VerfahrensId = c.Long(nullable: false),
+                        Objektart = c.String(nullable: false, maxLength: 255),
+                        Gemarkung = c.String(maxLength: 255),
+                        Flur = c.String(maxLength: 255),
+                        Wirtschaftsart = c.String(maxLength: 255),
+                        Anschrift = c.String(maxLength: 255),
+                        Groesse = c.String(maxLength: 50),
+                        Objekt = c.String(maxLength: 255),
+                        Blatt = c.String(maxLength: 50),
+                        Grundbuchamt = c.String(maxLength: 255),
+                        Zusatz = c.String(maxLength: 255),
+                        Eigentumsart = c.String(maxLength: 255),
+                        Nutzungsrecht = c.String(maxLength: 255),
+                        Kurzname = c.String(maxLength: 255),
+                        Verkehrswert = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Grundbuchbezirk = c.String(maxLength: 255),
+                        Eigentumsanteil = c.String(maxLength: 50),
+                        Schiffsregisterart = c.String(maxLength: 50),
+                        Schiffsname = c.String(maxLength: 255),
+                        IMONR = c.String(maxLength: 255),
+                        RegisterGericht = c.String(maxLength: 255),
+                    })
+                .PrimaryKey(t => t.ObjektId)
+                .ForeignKey("dbo.Verfahren", t => t.VerfahrensId, cascadeDelete: true)
+                .Index(t => t.VerfahrensId);
+            
+            CreateTable(
+                "dbo.ParteienBeteiligt",
+                c => new
+                    {
+                        ParteiId = c.Int(nullable: false, identity: true),
+                        VerfahrensId = c.Long(nullable: false),
+                        Partei = c.String(maxLength: 255),
+                    })
+                .PrimaryKey(t => t.ParteiId)
+                .ForeignKey("dbo.Verfahren", t => t.VerfahrensId, cascadeDelete: true)
+                .Index(t => t.VerfahrensId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ParteienBeteiligt", "VerfahrensId", "dbo.Verfahren");
+            DropForeignKey("dbo.Objekte", "VerfahrensId", "dbo.Verfahren");
+            DropIndex("dbo.ParteienBeteiligt", new[] { "VerfahrensId" });
+            DropIndex("dbo.Objekte", new[] { "VerfahrensId" });
+            DropTable("dbo.ParteienBeteiligt");
+            DropTable("dbo.Objekte");
+        }
+    }
+}
