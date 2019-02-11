45be0dae38f0db8284a3a1fa5478340cfa7b5758
Christoph Hessel <Christoph.Hessel@ovg.jm.rlp.de>
2019-02-11 09:34:30 +0100

    ds-suite - displays: saal-scroller-vsimm.component: scrollen der Objekte ermöglicht

--- src/ds-suite/apps/displays/src/app/templates/demo/saal-scroller-vsimm/saal-scroller-vsimm.component.ts
+++ src/ds-suite/apps/displays/src/app/templates/demo/saal-scroller-vsimm/saal-scroller-vsimm.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewChildren, ElementRef, QueryList  } from '@angular/core';
 import { trigger, state, style, animate, transition, stagger, query, keyframes } from '@angular/animations';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
 
 import { DisplayTemplateComponent } from '../../../display-template/display-template.component';
+import { Termin, Objekt } from '@ds-suite/model';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-saal-scroller-vsimm',
@@ -23,8 +27,84 @@ import { DisplayTemplateComponent } from '../../../display-template/display-temp
   ]
 })
 export class SaalScrollerVsimmComponent extends DisplayTemplateComponent {
+  public ScrollingObjectsActive: boolean;
+  private ScrollIntervallObjects: number;
+  private ScrollIntervallTimer: any;
+  private ScrollIntervallSub: Subscription;
+  private lastActiveTerminID: number = -1;
+
+  objects: Objekt[] = [];
+  objectsCount: number = 0;
+
+  @ViewChild('dsObjectsContainer') dsObjectsContainer: ElementRef;
+  @ViewChildren('dsObjectsChild') dsObjectsChildren: QueryList<ElementRef>;
+
   ngOnInit() {
     this.updateInterval=4000;
     super.ngOnInit();
+    this.initializeObjectScrolling();
   }
+
+  initializeObjectScrolling() {
+    this.ScrollIntervallObjects = 4000;
+    this.ScrollIntervallTimer = Observable.timer(2000, this.ScrollIntervallObjects);
+    this.ScrollIntervallSub = this.ScrollIntervallTimer.subscribe((t: any) => {
+      if (this.objects.length === 0 ||
+        (this.ScrollingObjectsActive && this.objects.length <= this.objectsCount) ||
+        (!this.ScrollingObjectsActive)) {
+        this.loadObjects();
+      } else {
+        if (this.ScrollingObjectsActive)
+          this.objects.shift();
+      }
+    });
+  }
+
+  isScrollingObjectsActive(): boolean {
+    if (this.dsObjectsContainer && this.dsObjectsChildren) {
+      const containerHeight = this.dsObjectsContainer.nativeElement.offsetHeight;
+      var childrenHeight = 0;
+
+      this.dsObjectsChildren.forEach(element => {
+        childrenHeight += element.nativeElement.offsetHeight;
+      });
+
+      return containerHeight < childrenHeight;
+    } else {
+      return false;
+    }
+  }
+
+  termineLoaded() {
+    super.termineLoaded();
+    this.ScrollingObjectsActive = this.isScrollingObjectsActive();
+    this.objectsCount = 0;
+    if (!isNullOrUndefined(this.aktiverTermin) && !isNullOrUndefined(this.aktiverTermin.objekte)) 
+      this.objectsCount = this.aktiverTermin.objekte.length;
+    if (isNullOrUndefined(this.aktiverTermin) || this.lastActiveTerminID != this.aktiverTermin.id) {
+      this.objects= [];
+      this.loadObjects();
+    }
+    
+    this.lastActiveTerminID = -1;
+    if (!isNullOrUndefined(this.aktiverTermin))
+      this.lastActiveTerminID = this.aktiverTermin.id;
+  }
+
+  loadObjects() {
+    var objects: Objekt[] = [];
+    if (!isNullOrUndefined(this.aktiverTermin) && !isNullOrUndefined(this.aktiverTermin.objekte))
+      objects=this.aktiverTermin.objekte;
+
+    if (this.ScrollingObjectsActive)
+      this.objects = this.objects.concat(objects);
+    else
+      this.objects = objects;
+  }
+
+  ngOnDestroy() {
+    this.ScrollIntervallSub.unsubscribe();
+    super.ngOnDestroy;
+  }
+
 }
