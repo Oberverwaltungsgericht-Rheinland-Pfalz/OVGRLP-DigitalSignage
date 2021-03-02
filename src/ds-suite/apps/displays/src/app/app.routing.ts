4cf43284c5552575d88aec8189314ac0c13c06ee
Reiner Bamberger <reiner.bamberger@ovg.jm.rlp.de>
2021-03-02 12:32:14 +0100

    Update auf Angular8 ohne Migration

--- src/ds-suite/apps/displays/src/app/app.routing.ts
+++ src/ds-suite/apps/displays/src/app/app.routing.ts
@@ -11,31 +11,31 @@ const routes: Routes = [
     children: [
       {
         path: "njzko",
-        loadChildren: './templates/njz-ko/njz-ko.module#NjzKoModule'
+        loadChildren: () => import('./templates/njz-ko/njz-ko.module').then(m => m.NjzKoModule)
       },
       {
         path: "njzkh",
-        loadChildren: './templates/njz-kh/njz-kh.module#NjzKhModule'
+        loadChildren: () => import('./templates/njz-kh/njz-kh.module').then(m => m.NjzKhModule)
       },
       {
         path: "demo",
-        loadChildren: './templates/demo/demo.module#DemoModule'
+        loadChildren: () => import('./templates/demo/demo.module').then(m => m.DemoModule)
       },
       {
         path: "jztr",
-        loadChildren: './templates/jz-tr/jz-tr.module#JzTrModule'
+        loadChildren: () => import('./templates/jz-tr/jz-tr.module').then(m => m.JzTrModule)
       },
       {
         path: "pfolg",
-        loadChildren: './templates/jz-pfolg/jz-pfolg.module#PfolgModule'
+        loadChildren: () => import('./templates/jz-pfolg/jz-pfolg.module').then(m => m.PfolgModule)
       },
       {
         path: "jzzw",
-        loadChildren: './templates/jz-zw/jz-zw.module#JzZwModule'
+        loadChildren: () => import('./templates/jz-zw/jz-zw.module').then(m => m.JzZwModule)
       },
       {
         path: "jzkl",
-        loadChildren: './templates/jz-kl/jz-kl.module#JzKlModule'
+        loadChildren: () => import('./templates/jz-kl/jz-kl.module').then(m => m.JzKlModule)
       }
     ]
   },
