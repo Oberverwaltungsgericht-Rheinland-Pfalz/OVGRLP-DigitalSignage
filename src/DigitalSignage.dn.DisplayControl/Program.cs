dcdd6ff2f57d79216db0be82a467579d5b62b885
Reiner Bamberger <reiner.bamberger@ovg.jm.rlp.de>
2023-11-10 14:12:29 +0100

    DisplayControl mit dotnet 8.0

--- src/DigitalSignage.dn.DisplayControl/Program.cs
+++ src/DigitalSignage.dn.DisplayControl/Program.cs
@@ -1,8 +1,33 @@
+// SPDX-FileCopyrightText: © 2023 Oberverwaltungsgericht Rheinland-Pfalz <poststelle@ovg.jm.rlp.de>
+// SPDX-License-Identifier: EUPL-1.2
+using DigitalSignage.dn.DisplayControl;
+using System.Runtime.InteropServices;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+IOperatingSystem os = DetectOS();
+
 app.UseFileServer();
 
-app.MapGet("/a", () => "Hello World!");
+app.MapGet("/api/shutdown", () => os.Shutdown);
+app.MapGet("/api/restart", () => os.Restart);
+app.MapGet("/api/screenshot", (HttpContext context) => os.Screenshot);
 
+app.UseHealthChecks("/health");
 app.Run();
+
+IOperatingSystem DetectOS()
+{
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        return new WindowsOperatingSystem();
+
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        return new LinuxOperatingSystem();
+
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        throw new NotImplementedException("MacOS ist zurzeit nicht implementiert!");
+
+    // Für den Fall, dass das Betriebssystem nicht gefunden werden kann
+    return null;
+}
